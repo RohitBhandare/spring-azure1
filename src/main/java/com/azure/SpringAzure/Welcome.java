454245a219441c2ef8fbbe0bbeddc1a8793cff7c
rohit <rohitbhandare7777@gmailcome>
2023-06-01 09:44:55 +0530

    Initial commit

---
+++ src/main/java/com/azure/SpringAzure/Welcome.java
@@ -0,0 +1,15 @@
+package com.azure.SpringAzure;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+
+    @RequestMapping("/hello")
+    public  String hello()
+    {
+        return "hello welcome";
+    }
+
+}
